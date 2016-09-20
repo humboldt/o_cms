664225f572c29811adb9e6a1396c006bdb1aaf9d
Benjamin Ayres <ben@outsidehq.co.uk>
2016-09-20 21:14:33 +0100

    Image views, validation and simple styles

--- app/models/o_cms/image.rb
+++ app/models/o_cms/image.rb
@@ -1,4 +1,8 @@
 module OCms
   class Image < ActiveRecord::Base
+    default_scope { order('created_at DESC') }
+
+    validates :name, length: { minimum: 5 }, presence: true
+    validates :file, length: { minimum: 5 }, presence: true
   end
 end
