5136d00494dc8bf2b09fd57c95522be2d94634e2
Benjamin Ayres <ben@outsidehq.co.uk>
2016-11-09 19:45:11 +0000

    Limit on Random data paragraph length

--- spec/dummy/lib/random_data.rb
+++ spec/dummy/lib/random_data.rb
@@ -4,7 +4,7 @@ module RandomData
     last_name = random_word.capitalize
     "#{first_name} #{last_name}"
   end
- 
+
   def self.random_email
     "#{random_word}@#{random_word}.#{random_word}"
   end
@@ -14,8 +14,8 @@ module RandomData
     rand(4..6).times do
       sentences << random_sentence
     end
-    
-    sentences.join(" ")
+
+    sentences.join(" ").first(250)
   end
 
   def self.random_sentence
